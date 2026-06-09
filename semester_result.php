6bab5b45a3ee79986440865587ef0afb9daf41e0
Nitin Singh <nitinsingh7325@gmail.com>
2026-06-09 12:23:31 +0530

    Add files via upload

---
+++ semester_result.php
@@ -0,0 +1,15 @@
+<?php
+// Constant
+define("COLLEGE_NAME", "ABC College");
+
+// Variables
+$studentName = "Rahul";
+$semester = 3;
+$percentage = 78.50;
+
+echo "<h3>Previous Semester Result</h3>";
+echo "College Name: " . COLLEGE_NAME . "<br>";
+echo "Student Name: " . $studentName . "<br>";
+echo "Semester: " . $semester . "<br>";
+echo "Percentage: " . $percentage . "%";
+?>
