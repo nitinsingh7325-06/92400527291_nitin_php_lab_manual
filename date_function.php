5671d88181a66e3c7b0d34cfe6c7bfa354d594b2
Nitin Singh <nitinsingh7325@gmail.com>
2026-07-14 10:23:50 +0530

    Add files via upload

---
+++ date_function.php
@@ -0,0 +1,12 @@
+<?php
+
+date_default_timezone_set("asia/kolkata");
+
+$date1 = date("d-M-y : H-i-s");
+
+
+echo $date1;
+$date_1 = new datetime("2026-7-7");
+$date_1 = new datetime("2021-7-7");
+echo
+?>
