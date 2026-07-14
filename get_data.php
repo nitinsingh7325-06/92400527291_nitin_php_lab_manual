1f42ba35d12dd5c4705627190bf329e38de5859c
Nitin Singh <nitinsingh7325@gmail.com>
2026-07-14 10:22:47 +0530

    Add files via upload

--- get_data.php
+++ get_data.php
@@ -1,8 +1,16 @@
 <?php
 
-echo "<pre>";
-print_r($_GET);
-echo "</pre>";
+$target_path = "uploads/"; //Location for uploadind files
+$target_path = $target_path . basename( $_FILES['fileToUploads']['name']);
 
+if(move_uploaded_file($_FILES['fileToUploads']['tmp_name'], $target_path)) {
+
+echo "File uploaded successfully!";
+
+} else{
+
+echo "Sorry, file not uploaded, please try again!";
+
+}
 
 ?>
